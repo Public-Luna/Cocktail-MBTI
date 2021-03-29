760ab97437bfabe4ce25bf12ff28f71edb0bef4b
moonk1212 <zm8208@naver.com>
2021-03-30 04:57:12 +0900

    hmm

--- public/javascripts/shareUrl.js
+++ public/javascripts/shareUrl.js
@@ -13,20 +13,6 @@ const shareData = {
         
     } catch(err) {
         $.get("/404");
-        /*const agent = navigator.userAgent.toLowerCase(); 
-        if(agent.indexOf('Trident')>-1){ alert("IE"); }
-        else if(agent.indexOf('safari')>-1){ alert("사파리"); }
-        else if(agent.indexOf('chrome')>-1){ alert("크롬"); }
-        else if(agent.indexOf('firefox')>-1){ alert("파이어폭스"); }
-        else if(agent.indexOf('opera')>-1){ alert("오페라"); }*/
 
-        const tempElem = document.createElement('textarea');
-        tempElem.value = window.location.href;  
-        document.body.appendChild(tempElem);
-      
-        tempElem.select();
-        document.execCommand("copy");
-        document.body.removeChild(tempElem);
-        alert('링크가 복사되었습니다!');
     }
     });
